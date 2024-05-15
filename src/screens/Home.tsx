ce03c5ee5266e66e445fdd828735e7475f749779
SL-23 <stephanieluvsphy@gmail.com>
2024-05-16 00:42:41 +0930

    Config test notification

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,41 @@
 import React, {useContext, useEffect} from 'react';
-import {ScrollView, View} from 'react-native';
+import {Alert, Button, ScrollView, View} from 'react-native';
 import TimeZoneSection from '../components/TimeZoneSection';
 import AddTimeZone from '../components/AddTimeZone';
 import MyTimeZonesContext from '../context/MyTimeZonesContext';
 import LocalTime from '../components/LocalTime';
 import {getBackgroundColor} from '../helpers/helpers';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Notifications from 'expo-notifications';
+
+const registerNotification = async () => {
+  let token;
+  const {status: existingStatus} = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== 'granted') {
+    const {status} = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+    Alert.alert('Failed to get push token for push notification!');
+    return;
+  }
+  // Learn more about projectId:
+  // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+  // EAS projectId is used here.
+  try {
+    const projectId = 'stephanieL.alarm';
+
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId,
+      })
+    ).data;
+    console.log(token, 'DONE');
+  } catch (e) {
+    token = `${e}`;
+  }
+};
 
 const Home = () => {
   const {myTimeZones, setSavedTimeZones} = useContext(MyTimeZonesContext);
@@ -38,6 +68,32 @@ const Home = () => {
     getData();
   }, [myTimeZones]);
 
+  useEffect(() => {
+    registerNotification();
+    console.log('RE');
+  }, []);
+
+  const schedulePushNotification = async () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: {data: 'goes here', test: {test1: 'more data'}},
+      },
+      trigger: {seconds: 2},
+    })
+      .then(r => {
+        console.log({r}, 'result');
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const handleClick = () => {
+    console.log('KK');
+    schedulePushNotification();
+  };
   return (
     <ScrollView
       contentInsetAdjustmentBehavior="automatic"
@@ -45,6 +101,7 @@ const Home = () => {
         backgroundColor,
         height: '100%',
       }}>
+      <Button title="Test notification" onPress={handleClick} />
       <AddTimeZone />
       <LocalTime timeZone={timeZone} currentTime={currentTime} />
       <View>
